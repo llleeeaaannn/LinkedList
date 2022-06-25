3ba6d299b044d8a14691574e15242543aa4b2ca6
llleeeaaannn <piercefrank10@gmail.com>
2022-06-25 12:59:02 +0100

    Completed entire singly linked list data structure

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
 # LinkedList
+This repository is my implementation of a Linked list data structure through Ruby which I undertook as part of The Odin Project. I implemented a class for the linked list itself while also creating a class representing the nodes found in the list. The linked list is singly linked. I also added several useful methods to the linked list class as shown below.
+
+# Guide for use
+To use this data structure, simply include it in your file, and create an instance of it as you would with a hash or an array; all methods can be called directly on the instance of the class.
+In order to make use of this data stricture simply include it in your file and create an instance of the Linked List. All methods can be called directly on the linked list class.
+
+# Methods
+| Methods           | Functionality |
+| ----------------- | :---------- |
+| #append(value)             | Adds a new node containing value to the end of the list |
+| #prepend(value)            | Adds a new node containing value to the beginning of the list |
+| #insert_at(value, index)   | Adds a new node containing value to the selected index of the list |
+| #size                      | Returns the total number of nodes in the list |
+| #head                      | Returns the first node in the list |
+| #tail                      | Returns the last node in the list |
+| #at(index)                 | Returns the node at the given index |
+| #pop                       | Removes the last element from the list |
+| #remove_at(index)          | Removes the node at the selected index from the list |
+| #contains?(value)          | Returns true if the passed in value is in the list and otherwise returns false |
+| #find(value)               | Returns the index of the node containing value, or nil if not found |
+| #to_s                      | Prints a string representation of the list |
---
+++ linked_list.rb
@@ -0,0 +1,166 @@
+class LinkedList
+
+  attr_reader :head, :tail
+
+  def initialize
+    @head = nil
+    @tail = nil
+  end
+
+  def append(value)
+    new_node = Node.new(value)
+
+    if @head.nil?
+      @head = new_node
+    elsif @tail.nil?
+      @head.next_node = new_node
+      @tail = new_node
+    else
+      @tail.next_node = new_node
+      @tail = new_node
+    end
+  end
+
+  def prepend(value)
+    new_node = Node.new(value)
+
+    if @head.nil? && @tail.nil?
+      @head = new_node
+    elsif !@head.nil? && @tail.nil?
+      @tail = @head
+      @head = new_node
+      @head.next_node = @tail
+    else
+      new_node.next_node = @head
+      @head = new_node
+    end
+  end
+
+  def insert_at(value, index)
+    size = self.size
+    return nil if index > size
+
+    new_node = Node.new(value)
+
+    if index.zero?
+      new_node.next_node = @head
+      @head = new_node
+    elsif tail.nil?
+      @tail = new_node
+      @head.next_node = @tail
+    elsif index == size
+      @tail.next_node = new_node
+      @tail = new_node
+    else
+      current = self.at(index, 1)
+      new_node.next_node = current.next_node
+      current.next_node = new_node
+    end
+  end
+
+  def size
+    i = 0
+    current = @head
+    until current.nil?
+      current = current.next_node
+      i += 1
+    end
+    i
+  end
+
+  def at(index, i = 0)
+    current = @head
+    while i < index
+      current = current.next_node
+      i += 1
+    end
+    current
+  end
+
+  def pop
+    size = self.size
+    if size == 1
+      @head = nil
+    elsif size == 2
+      @head.next_node = nil
+      @tail = nil
+    elsif size > 2
+      current = @head
+      size -= 2
+      size.times do
+        current = current.next_node
+      end
+      current.next_node = nil
+      @tail = current
+    end
+  end
+
+  def remove_at(index)
+    size = self.size
+    return nil if index >= size
+
+    if index.zero?
+      if tail.nil?
+        @head = nil
+      elsif size == 2
+        @head = tail
+        @tail = nil
+      else
+        @head = @head.next_node
+      end
+    elsif index == size - 1
+      current = self.at(index, 1)
+      current.next_node = nil
+      @tail = current
+    else
+      current = self.at(index, 1)
+      after_removed_node = current.next_node.next_node
+      current.next_node = nil
+      current.next_node = after_removed_node
+    end
+  end
+
+  def contains?(value)
+    output = false
+    current = @head
+    while !current.nil? && !output
+      current.value == value ? output = true : nil
+      current = current.next_node
+    end
+    output
+  end
+
+  def find(value)
+    output = false
+    index = -1
+    current = @head
+    while !current.nil? && !output
+      current.value == value ? output = true : nil
+      current = current.next_node
+      index += 1
+    end
+    output ? index : nil
+  end
+
+  def to_s
+    array = []
+    current = @head
+    until current.nil?
+      array.push("( #{current.value} )")
+      current = current.next_node
+    end
+    array.push('nil').join(' -> ')
+  end
+
+end
+
+class Node
+
+  attr_accessor :value, :next_node
+
+  def initialize(value, next_node = nil)
+    @value = value
+    @next_node = next_node
+  end
+
+end
